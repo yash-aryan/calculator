d46b62324d412ee28086ae2207d1009ec7ccbcca
Yash <79582883+yash-aryan@users.noreply.github.com>
2023-04-10 21:10:43 +0530

    Rework entire JavaScript logic & Update the UI
    Updated almost entire scripts.js with different logic for calculator to
    follow the constraints/gotchas of the project. Now uses num1 & num2 only
    to store numbers and operate.
    Updated index.html & styles.css for a newer UI, and added more buttons
    which will be given functionality in later versions.

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# calculator
+# Sequential Calculator - Overview
 
 This is a project I created while doing The Odin Project's foundations course.
 
 It features a sequential operating calculator, that performs the basic arithmetic operations on recent inputs. By recent inputs, it means that the calculator does not follow BODMAS/PEMDAS rule, because output of previous operation becomes one of the inputs for next operations.
 
-For example, equations like `2*2 + 3*3` will result in `21` instead of `13`; this way of calculating was instructed in the course itself.
+For example, equations like `2 x 2 + 3 x 3` will result in `21` instead of `13`; this way of calculating was instructed in the course itself.
+
+# Challenges Faced
+
+This was one of the hardest projects for me, and I spent months upon this to improve my fundamentals in Vanilla JS and CSS Flexbox
+- Understanding and applying the knowledge of all the changes that happen on every next click, due to the nature of this type of calculator
+- Prevent edge cases bugs that happens due to user providing an unexpected input
+- Navigate through [the constraints](https://www.theodinproject.com/lessons/foundations-calculator#assignment) provided by The Odin Project.
--- scripts.js
+++ scripts.js
@@ -4,97 +4,146 @@
 const numbers_DOM = document.querySelectorAll("#number");
 const operators_DOM = document.querySelectorAll("#operator");
 const equals_DOM = document.querySelector("#equals");
-const clear_DOM = document.querySelector("#clear");
+const allClear_DOM = document.querySelector("#clear");
 const decimal_DOM = document.querySelector("#decimal");
 const screenTop_DOM = document.querySelector(".screen-top");
-const screenBtm_DOM = document.querySelector(".screen-bottom");
+const screenBottom_DOM = document.querySelector(".screen-bottom");
 
 // Global Variables
-let index;
-let tempNumber = "";
-let tempOperator = "";
-let numbersArr = [];
-let operatorsArr = [];
+let num1 = "";
+let num2 = "";
+let currOperator = null;
+let nextOperator = null;
+let screenHasToBeReset = false; // Signifies if it is time for the user to choose the 2nd operand
 
 // --- MAIN CODE ---
-numbers_DOM.forEach(n => n.addEventListener('click', storeOperatorInArr));
-operators_DOM.forEach(n => n.addEventListener('click', storeNumberInArr));
-equals_DOM.addEventListener('click', dispResultOnScreen);
-clear_DOM.addEventListener('click', resetValues);
-
+fullReset();
+numbers_DOM.forEach(n => n.addEventListener("click", runOnNumberKey));
+operators_DOM.forEach(n => n.addEventListener("click", runOnOperatorKey));
+equals_DOM.addEventListener("click", runOnEqualsKey);
+allClear_DOM.addEventListener("click", fullReset);
 
 // --- FUNCTION DECLARATIONS ---
-function storeOperatorInArr(e) {
-  tempNumber += e.target.textContent;
-  screenTop_DOM.textContent += e.target.textContent;
-  if (tempOperator) {
-    operatorsArr.push(tempOperator);
-    tempOperator = "";
+function runOnNumberKey(e) {
+  // Resets screen for new operation, after the result of previous one had ended through equals key (=)
+  if (screenTop_DOM.textContent !== "\xa0" && currOperator === null) {
+    screenTop_DOM.textContent = "\xa0";
+    screenBottom_DOM.textContent = "\xa0";
+  }
+  // Resets bottom screen only if the value is true
+  if (screenHasToBeReset) {
+    screenBottom_DOM.textContent = "\xa0";
+    screenHasToBeReset = false;
+  }
+  // Appends the number to the bottom screen
+  if (screenBottom_DOM.textContent.length <= 5) {
+    screenBottom_DOM.textContent += e.target.textContent;
   }
 }
 
-function storeNumberInArr(e) {
-  tempOperator = e.target.value;
-  screenTop_DOM.textContent += e.target.textContent;
-  if (tempNumber) {
-    numbersArr.push(+tempNumber);
-    tempNumber = "";
+function runOnOperatorKey(e) {
+  // If the first thing user clicks is an operator, this makes sure it does nothing
+  if (screenBottom_DOM.textContent === "\xa0") {
+    return;
+  }
+  else if (!screenHasToBeReset) {
+    // When chaining operators, stores selected operator in nextOperator
+    if (num1 !== "" && currOperator !== null) {
+      num2 = screenBottom_DOM.textContent;
+      nextOperator = e.target.value;
+      num1 = operate(+num1, +num2, currOperator);
+      screenBottom_DOM.textContent = num1;
+      screenTop_DOM.textContent = `${num1} ${currOperator}`;
+      screenHasToBeReset = true;
+    }
+    // Run only for the first time after clear all & equals
+    else {
+      num1 = screenBottom_DOM.textContent;
+      currOperator = e.target.value;
+      screenTop_DOM.textContent = `${num1} ${currOperator}`;
+      screenHasToBeReset = true;
+    }
+  }
+  // Changes to the newest selected operator, if user clicks operator keys consecutively
+  else {
+    currOperator = e.target.value;
+    screenTop_DOM.textContent = `${num1} ${currOperator}`;
+  }
+
+}
+
+function runOnEqualsKey() {
+  // If user doesn't choose num2 & presses equals
+  if (currOperator === null || screenHasToBeReset) {
+    screenTop_DOM.textContent = "\xa0";
+  }
+  // Takes num2 & operates & reset values for next operation
+  else {
+    num2 = screenBottom_DOM.textContent;
+    screenTop_DOM.textContent = `${+num1} ${currOperator} ${+num2}`;
+    num1 = operate(+num1, +num2, currOperator);
+    screenBottom_DOM.textContent = num1;
+    num2 = "";
+    currOperator = null;
+    nextOperator = null;
   }
 }
 
 function dispResultOnScreen() {
-  if (tempNumber) {
-    numbersArr.push(+tempNumber);
-    tempNumber = "";
+  screenBottom_DOM.textContent = num1;
+}
+
+function fullReset() {
+  num1 = "";
+  num2 = "";
+  currOperator = null;
+  nextOperator = null;
+  screenTop_DOM.textContent = "\xa0";
+  screenBottom_DOM.textContent = "\xa0";
+}
+
+function operate(num1, num2, op) {
+  switch (op) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "×":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      console.log("Invalid Operator");
+      return;
   }
-  screenBtm_DOM.textContent = `${operate()}`;
 }
 
-function resetValues() {
-  tempNumber = "";
-  tempOperator = "";
-  numbersArr = [];
-  operatorsArr = [];
-  screenTop_DOM.textContent = '\xa0';
-  screenBtm_DOM.textContent = '\xa0';
-}
-
-function operate() {
-  index = 0;
-  let result = numbersArr.reduce((a, b) => {
-    switch (operatorsArr[index]) {
-      case "+":
-        return add(a, b);
-      case "-":
-        return subtract(a, b);
-      case "*":
-        return multiply(a, b);
-      case "/":
-        return divide(a, b);
-      default:
-        console.log("Invalid Operator");
-        return;
-    }
-  });
+function add(num1, num2) {
+  const result = num1 += num2;
+  updateVariables();
   return result;
 }
 
-function add(a, b) {
-  index++;
-  return a += b;
+function subtract(num1, num2) {
+  const result = num1 -= num2;
+  updateVariables();
+  return result;
 }
 
-function subtract(a, b) {
-  index++;
-  return a -= b;
+function multiply(num1, num2) {
+  const result = num1 *= num2;
+  updateVariables();
+  return result;
 }
 
-function multiply(a, b) {
-  index++;
-  return a *= b;
+function divide(num1, num2) {
+  // if (num2 === 0) 
+  const result = num1 /= num2;
+  updateVariables();
+  return result;
 }
 
-function divide(a, b) {
-  index++;
-  return a /= b;
+function updateVariables() {
+  num2 = "";
+  currOperator = nextOperator;
 }
